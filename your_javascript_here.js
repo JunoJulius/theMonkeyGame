61b91983ee5a96b0c5433540605e331412dde66c
JunoJulius <yunoyulius@gmail.com>
2018-06-03 15:07:14 +0200

    created front-end, working on logic

---
+++ your_javascript_here.js
@@ -0,0 +1,26 @@
+// Variables
+const hero = {
+  name: 'Julius',
+  heroic: true,
+  inventory: [{type: "Fermented banana", damage: 10 },
+              {type: "Prepared Statement", damage: 5}],
+  health: 5,
+  weapon: {type: "Laptop", damage: 8,
+  }
+}
+
+
+const nemesis = {
+  name: 'apiGong',
+  badAss: true,
+  inventory: [],
+  health: 0,
+  item: {type: "SQLinjections", damage: 10,
+  },
+}
+
+
+// Game logic
+
+
+// UI
